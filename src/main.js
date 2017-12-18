876f3ff2d33212cb968841c2e428d595191c657d
Ignorantcrazy <le_crazy@126.com>
2017-12-18 17:25:04 +0800

    mian

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+import test from './test';
+
+ReactDOM.render(<test />, document.getElementById('root'));
--- webpack.common.js
+++ webpack.common.js
@@ -6,14 +6,41 @@ module.exports = {
     entry:{
         main:'./src/main.js'
     },
-    plugin:[
+    plugins:[
         new cleanWebpackPlugin(['dist']),
         new htmlWebpackPlugin({
-            title:'RRRSWQL'
+            title: 'RRRSWQL',
+            template: 'template/template.html',
+            favicon:'./src/AppIcon.ico',
+            filename:'main.html',
         })
     ],
     output:{
         filename:'[name].bundle.js',
         path : path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ],
+                exclude:/node_module/
+            },
+            {
+                test:/\.(png|svg|jpb|gif)$/,
+                use:['file-loader'],
+                exclude:/node_module/
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: [
+                'file-loader'
+              ],
+              exclude:/node_module/
+            }
+        ]
     }
 };
